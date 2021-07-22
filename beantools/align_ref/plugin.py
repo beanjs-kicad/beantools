f7bfe228dfb69a3b9de88cc2b21c34469daf7393
WangJun <445212619@qq.com>
2021-07-22 11:36:54 +0800

    1.update align ref

--- beantools/align_ref/plugin.py
+++ beantools/align_ref/plugin.py
@@ -1,8 +1,9 @@
 import wx
-
+import pcbnew
 
 from os import path
-from pcbnew import ActionPlugin,GetBoard,B_SilkS,F_SilkS,User_1,User_2
+from pcbnew import ActionPlugin,GetBoard
+
 
 class AlignRefPlugin(ActionPlugin):
     """Class that gathers the actionplugin stuff"""
@@ -19,29 +20,60 @@ class AlignRefPlugin(ActionPlugin):
         designSettings=board.GetDesignSettings()
 
         for fp in footprints:
-            fpCenter=fp.GetCenter()
             ref=fp.Reference()
             layerId=ref.GetLayer()
 
-            if layerId==User_1 or layerId==User_2:
+            if layerId==pcbnew.User_1 or layerId==pcbnew.User_2:
                 continue
 
-            if layerId == F_SilkS:
-                ref.SetLayer(User_1)
+            if layerId == pcbnew.F_SilkS:
+                ref.SetLayer(pcbnew.User_1)
             
-            if layerId == B_SilkS:
-                ref.SetLayer(User_2)
+            if layerId == pcbnew.B_SilkS:
+                ref.SetLayer(pcbnew.User_2)
 
             textSize=designSettings.GetTextSize(layerId)
             ref.SetTextThickness(designSettings.GetTextThickness(layerId))
             ref.SetTextWidth(textSize.GetWidth())
             ref.SetTextHeight(textSize.GetHeight())
+
+
+            fpBoundingBox=self.footprintBoundingBox(fp)
+            fpCenter=fpBoundingBox.GetCenter()
             ref.SetPosition(fpCenter)
 
         wx.MessageBox("align finished!!")
-            
+        pcbnew.Refresh()
+    
+    def footprintBoundingBox(self,fp):
+        fpLayerId=fp.GetLayer()
 
+        ctrLayerId=pcbnew.F_CrtYd
+        if fpLayerId== pcbnew.B_Cu :
+            ctrLayerId=pcbnew.B_CrtYd
 
+        gItems = fp.GraphicalItems()
+
+        minx=1000000000000
+        miny=1000000000000
+        maxx=-1
+        maxy=-1
+        for item in gItems:
+            if item.GetLayer() == ctrLayerId:
+                iBox=item.GetBoundingBox()
+                if iBox.GetLeft() < minx:
+                    minx=iBox.GetLeft()
+                
+                if iBox.GetRight() > maxx:
+                    maxx=iBox.GetRight()
+
+                if iBox.GetTop() < miny:
+                    miny=iBox.GetTop()
+                
+                if iBox.GetBottom() > maxy:
+                    maxy=iBox.GetBottom()
+
+        return pcbnew.EDA_RECT(pcbnew.wxPoint(minx,miny),pcbnew.wxSize(maxx-minx,maxy-miny))
 
 
 
